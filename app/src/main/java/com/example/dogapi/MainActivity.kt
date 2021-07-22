6e762c6e368fe3fa22bee476b3d25da83edf1f34
ignaciocervino <ignaciocervino@gmail.com>
2021-07-22 01:44:32 -0300

    Adding corroutines

--- app/src/main/java/com/example/dogapi/MainActivity.kt
+++ app/src/main/java/com/example/dogapi/MainActivity.kt
@@ -4,6 +4,11 @@ import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.dogapi.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding//viewBinding es la forma de enlazar las listas al codigo
@@ -12,4 +17,27 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
+    //Retrofit
+    /*ESte objeto va a tener la URL original, el conversor de JSON al modelo de datos y toda la configuracion para hacer llamadas a internet*/
+    private fun getRetrofit():Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/breed/")
+            .addConverterFactory(GsonConverterFactory.create())//Convierte el JSON a DogResponse
+            .build()
+    }
+
+
+    private fun searchByName(query:String){
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(APIService::class.java).getDogsByBreeds("$query/images")
+            val puppies = call.body()
+            if(call.isSuccessful){
+                //show Recyclerview
+            }else{
+                //show error
+            }
+        }
+    }
 }
+
