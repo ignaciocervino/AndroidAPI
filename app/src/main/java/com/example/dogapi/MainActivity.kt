2adcc14138471d8d471d4d6163aca6e2dd378627
ignaciocervino <ignaciocervino@gmail.com>
2021-07-22 01:15:09 -0300

    Setting viewBinding

--- app/src/main/java/com/example/dogapi/MainActivity.kt
+++ app/src/main/java/com/example/dogapi/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.dogapi
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dogapi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding//viewBinding es la forma de enlazar las listas al codigo
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
